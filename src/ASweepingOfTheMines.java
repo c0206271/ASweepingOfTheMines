59dbf383d254bf04c528d0c2c8ca3aff71d027c3
KHSCS078 <KHSCS078@KATYISD.ORG>
2018-10-03 10:22:06 -0500

    added event handlers

--- src/ASweepingOfTheMines.java
+++ src/ASweepingOfTheMines.java
@@ -9,6 +9,7 @@ import javafx.scene.shape.Line;
 import javafx.scene.paint.Color;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
@@ -35,6 +36,8 @@ public class ASweepingOfTheMines extends Application
 {@Override
     public void start(Stage stage)
     {
+ ///////////////////////////////SETTING THE GRID//////////////////////
+
         GridPane grid= new GridPane();
         Button[][] buttons =new Button[16][16];
 
@@ -42,7 +45,7 @@ public class ASweepingOfTheMines extends Application
             for(int y=0; y<16;y++)
             {
                 Button b1=new Button("iii");
-                b1.setTranslateX(100.0);
+                b1.setTranslateX(200.0);
                 buttons[x][y]= b1;
             }
         for(int x=0; x<16; x++)
@@ -51,15 +54,52 @@ public class ASweepingOfTheMines extends Application
         grid.setHgap(0);
         grid.setVgap(0);
 
+/////////////////////////////SETTING THE OBJECTS/TEXT/////////////////////////////
+        Text text1 = new Text();
+        text1.setText("Hello and welcome to A Sweeping Of The Mines. Press enter to continue to the game.");
+        text1.setX(100);
+        text1.setY(450);
+        text1.setFont(Font.font("arial", 14));
+        text1.setStroke(Color.BLACK);
+
+//////////////////////////////VARIABLES///////////////////////////////////////////////////////
+int choose[]={0};
+int[][]mines=new int[16][16];
+
+ ////////////////////////////EVENT HANDLER/ STANDARD STUFF///////////////////////////////
         stage.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+            if(choose[0]==0)
+            {
+                if(key.getCode()==KeyCode.ENTER)
+                {
+                    text1.setText("You must now clear this Field of Mines.");
+                    choose[0]++;
+                }
+
+            }
+            if(choose[0]==2)
+            {
+
+            }
+
+
 
         });
-        Group root = new Group(grid);
-        Scene scene = new Scene(root, 640, 480);
+        stage.addEventHandler(MouseEvent.MOUSE_CLICKED, (mouse) -> {
+            if(choose[0]==2)
+            {
+
+            }
+        });
+ ///////////////////////////GROUP/STANDARD/////////////////////////////////////////////////////
+        Group root = new Group(grid, text1);
+        Scene scene = new Scene(root, 800, 600);
         stage.setTitle("A Sweeping Of the Mines");
         stage.setScene(scene);
         stage.show();
     }
+
+/////////////////////////////////RUNNING THE PROGRAM/////////////////////////////////////////////////////////
     public static void main(String args[]){
         launch(args);
 
